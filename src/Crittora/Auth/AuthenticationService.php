b886159a5a549e657aabce375172d9a293603a4e
Gerardo I. Ornelas <gerardo@ornelastechnologies.com>
2024-12-20 13:09:24 -0600

    add loggin to SDK

--- src/Crittora/Auth/AuthenticationService.php
+++ src/Crittora/Auth/AuthenticationService.php
@@ -36,6 +36,8 @@ class AuthenticationService
 
     public function authenticate(string $username, string $password): array
     {
+        error_log("Authenticating user with username: $username");
+
         try {
             $result = $this->client->initiateAuth([
                 'AuthFlow' => 'USER_PASSWORD_AUTH',
@@ -46,12 +48,18 @@ class AuthenticationService
                 ],
             ]);
 
+            error_log("Authentication successful for user: $username");
+            error_log("IdToken: " . $result['AuthenticationResult']['IdToken']);
+            error_log("AccessToken: " . $result['AuthenticationResult']['AccessToken']);
+            error_log("RefreshToken: " . $result['AuthenticationResult']['RefreshToken']);
+
             return [
                 'IdToken' => $result['AuthenticationResult']['IdToken'],
                 'AccessToken' => $result['AuthenticationResult']['AccessToken'],
                 'RefreshToken' => $result['AuthenticationResult']['RefreshToken'],
             ];
         } catch (AwsException $e) {
+            error_log("Authentication failed for user: $username with error: " . $e->getMessage());
             throw new Exception('Authentication failed: ' . $e->getMessage());
         }
     }
